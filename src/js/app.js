4957166bcca45e5c644a0214162252c73dbcf6b2
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2017-12-19 13:29:45 +0200

    !

--- src/js/app.js
+++ src/js/app.js
@@ -27,6 +27,48 @@ $(function() {
   	}
   });
 
+  //Мобильная менюшка
+  var slideout = new Slideout({
+    'panel': document.getElementById('panel'),
+    'menu': document.getElementById('menu'),
+    'padding': 256,
+    'tolerance': 70,
+  });
+  document.querySelector('.menu-open').addEventListener('click', function() {
+    slideout.open();
+  });
+  document.querySelector('.menu-close').addEventListener('click', function() {
+    slideout.close();
+  });
+
+  function close(eve) {
+    eve.preventDefault();
+    slideout.close();
+  }
+  slideout.on('open', function() { 
+    $('#btn').addClass('on');
+  });
+
+  slideout.on('close', function() { 
+    $('#btn').removeClass('on');
+  });
+  // Сайт затемняет
+  function close(eve) {
+    eve.preventDefault();
+    slideout.close();
+  }
+
+  slideout
+    .on('beforeopen', function() {
+      this.panel.classList.add('panel-open');
+    })
+    .on('open', function() {
+      this.panel.addEventListener('click', close);
+    })
+    .on('beforeclose', function() {
+      this.panel.classList.remove('panel-open');
+      this.panel.removeEventListener('click', close);
+    });
 
 
 
