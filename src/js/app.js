09e75644282bf50f29ec44b2b3e2fd123474c6ab
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2017-12-19 13:06:25 +0200

    mobail

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,21 @@ $(function() {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+	    1024: {
+	      slidesPerView: 4,
+	      spaceBetween: 10
+    	},
+    	900: {
+	      slidesPerView: 3,
+	      spaceBetween: 10
+    	},
+    	767: {
+	      slidesPerView: 2,
+	      spaceBetween: 34,
+	      slidesPerColumn: 2
+    	},
+  	}
   });
 
 
