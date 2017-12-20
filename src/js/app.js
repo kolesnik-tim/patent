e133065e484d6cf57f1c53264953fc056e627ec3
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2017-12-20 16:22:29 +0200

    section work

--- src/js/app.js
+++ src/js/app.js
@@ -40,6 +40,7 @@ $(function() {
   $('.menu-close').on('click', function() {
     slideout.close();
   });
+  //Мобильная менюшка (Затемнение)
   slideout
     .on('beforeopen', function() {
       this.panel.classList.add('panel-open');
@@ -51,6 +52,16 @@ $(function() {
       this.panel.classList.remove('panel-open');
       this.panel.removeEventListener('click', close);
     });
+
+  // Якорь
+  $(document).ready(function() {
+    $('.menu').on('click','a', function(event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 1500);
+    });
+  });
   // форма
   $('.btn').on('click', function() {
     $('.slideout-panel').addClass('is-active');
@@ -59,7 +70,25 @@ $(function() {
   $('.form__close').on('click', function() {
     $('.slideout-panel').removeClass('is-active');
   });
+  // input[typu='file']
+  var wrapper = $( '.form__block__file' ),
+    inp = wrapper.find( 'input' ),
+    lbl = wrapper.find( 'label' ),
+    file_api = ( window.File && window.FileReader && window.FileList && window.Blob ) ? true : false;
+  inp.change(function() {
+    var file_name;
+    if( file_api && inp[ 0 ].files[ 0 ] )
+      file_name = inp[ 0 ].files[ 0 ].name;
+    else
+      file_name = inp.val().replace( 'C:\\fakepath\\', '' );
 
+    if( ! file_name.length )
+      return;
+
+    if( lbl.is( ':visible' ) ) {
+      lbl.text( file_name );
+    }
+  }).change();
 
 
 
